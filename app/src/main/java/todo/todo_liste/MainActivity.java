1ee41ccc56efbf16bdf28d8c1b4bed0965e8b0df
Shizanu <tobias.rojahn@gmx.net>
2017-04-17 10:47:07 +0200

    Test-Commit

--- app/src/main/java/todo/todo_liste/MainActivity.java
+++ app/src/main/java/todo/todo_liste/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
+     * Tobias was here
      */
     private GoogleApiClient client;
 
